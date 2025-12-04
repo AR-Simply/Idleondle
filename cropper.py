db2b88b858c6144964f5952107d1cb398388ec53
Adamsen5482 <tadams17@student.aau.dk>
2025-12-04 14:17:08 +0100

    h

---
+++ cropper.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+
+# configure these values
+input_folder = "C:/Users/tobia/PycharmProjects/wiki scraper/images/recipes/Tab 6"
+output_folder =  "C:/Users/tobia/PycharmProjects/wiki scraper/images/recipes/output"
+
+# crop parameters
+x = 577           # top-left x
+y = 261            # top-left y
+width = 523
+height = 632
+
+os.makedirs(output_folder, exist_ok=True)
+
+for filename in os.listdir(input_folder):
+    if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+        img_path = os.path.join(input_folder, filename)
+        img = Image.open(img_path)
+
+        crop_box = (x, y, x + width, y + height)
+        cropped = img.crop(crop_box)
+
+        out_path = os.path.join(output_folder, filename)
+        cropped.save(out_path)
