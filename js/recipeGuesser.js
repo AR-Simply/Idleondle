a97fe729ce967b9971fd1ea28c4592fdff22217a
GrovHubs <120721337+GrovHubs@users.noreply.github.com>
2025-12-09 00:18:40 +0100

    Update recipeGuesser.js

--- js/recipeGuesser.js
+++ js/recipeGuesser.js
@@ -55,7 +55,7 @@ export async function initRecipeGuesser(options = {}) {
   const sharedConfig = Object.assign({
     dataUrl: '../json/idleon_recipes.json',
     imageBase: '../images',
-    seedOffset: 3,
+    seedOffset: 4,
     guessButtonHandlers: {
       guessBtn2: () => {
         const gb2 = document.getElementById('guessBtn2');
